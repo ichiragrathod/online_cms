9ea7cb56215e0af03a0e6aaa95568b88a5d89189
chirag <chirag.r@simformsolutions.com>
2022-06-08 14:32:01 +0530

    User Dashboard Created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'dashboard#index', as: 'dashboard'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
