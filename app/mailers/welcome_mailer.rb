e04e1c5105c03d30d31207c7d73551ff17504a34
chirag <chirag.r@simformsolutions.com>
2022-06-08 13:38:58 +0530

    Implemented Action Mailer.

---
+++ app/mailers/welcome_mailer.rb
@@ -0,0 +1,7 @@
+class WelcomeMailer < ApplicationMailer
+  def send_greetings_notification(user)
+    @user = user
+    mail to: @user.email,
+    subject: "Thank you for signing up on out site"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :trackable   
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable 
+  
+  private
+  def after_confirmation 
+    WelcomeMailer.send_greetings_notification(self).deliver_now
+  end
 end
